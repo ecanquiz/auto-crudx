d5c37c0d44b4bd57505e59dfbb1cc825f5e9719a
Ernesto Canquiz <fdsoil123@gmail.com>
2023-10-29 16:25:21 -0400

    deploy

--- assets/install.md.5465bb02.lean.js
+++
@@ -1,1 +0,0 @@
-import{_ as s,o as a,c as n,S as l}from"./chunks/framework.233e4693.js";const d=JSON.parse('{"title":"Instalación","description":"","frontmatter":{},"headers":[],"relativePath":"install.md","filePath":"install.md"}'),o={name:"install.md"},p=l("",3),e=[p];function t(c,r,C,y,i,A){return a(),n("div",null,e)}const _=s(o,[["render",t]]);export{d as __pageData,_ as default};
